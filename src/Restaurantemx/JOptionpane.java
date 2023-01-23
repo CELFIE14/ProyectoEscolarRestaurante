8628a65664c483cf7286539277b0f3e5becb60db
CELFIE14 <osiriscelf4@hotmail.com>
2023-01-23 00:20:16 -0600

    Primera versión de restaurante-ejercicio

---
+++ src/Restaurantemx/JOptionpane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Restaurantemx;
+
+/**
+ *
+ * @author osiri
+ */
+class JOptionpane {
+
+    static void ShowMessageDialog(Object object, String quiere_ingresar_un_producto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
